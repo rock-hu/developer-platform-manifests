b6214cca21cc0035dd358e6e8ce85ebd12782b6c
Rock Hu <rock.c.c.hu@gmail.com>
2026-04-12 11:38:24 +0800

    docs: redocly split paths

--- docs/OpenBankingUK/read-write-api-specs/v4.0.0/paths.sh
+++ docs/OpenBankingUK/read-write-api-specs/v4.0.0/paths.sh
@@ -1,5 +1,22 @@
 #! /bin/bash
+
+export domain=$1
+
+# ./paths.sh account-transaction
+# ./paths.sh confirmation-funds
+# ./paths.sh event-notifications
+# ./paths.sh events
+# ./paths.sh payment-initiation
+# ./paths.sh variable-recurring-payments
+
+
+
+rm -rf ${domain}/*-openapi.yaml
+
 while IFS="," read -r service path
 do
-    echo "${service}"
-done <  payment-initiation-paths.csv
+    echo "${service} ${path}"
+    touch ${domain}/${service}-openapi.yaml
+    echo "  /${path}:" >> ${domain}/${service}-openapi.yaml
+    echo "    \$ref: paths/${path}.yaml" >> ${domain}/${service}-openapi.yaml
+done <  ${domain}-paths.csv
